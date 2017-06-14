c4bd3bc10c5969d7e4a64c8a92e7bf8f1d6314d0
Daniel DeFlavia <dcd@intovpc.com>
2017-06-13 22:08:03 -0400

    bugfixes

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var config = require('./config'),
-  app = require('./app')(config);
+var config = require('./config');
+
+if (!!process.env.PORT)
+  config.port = process.env.PORT;
+
+if (!!process.env.REDIS_HOST) 
+  config.redis.host = process.env.REDIS_HOST;
+
+if (!!process.env.REDIS_PORT) 
+  config.redis.port = process.env.REDIS_PORT;
+
+var app = require('./app')(config);
 
 app.listen(config.port, function() {
   console.log("Matador listening on port", config.port, "in", process.env.NODE_ENV, "mode");
